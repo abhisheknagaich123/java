981b28087a7f37a627eb58d586943486555b5f00
Abhishek Nagaich <77282305+abhisheknagaich123@users.noreply.github.com>
2021-12-04 17:28:22 +0530

    Create check_Valid_Email_Address.java

---
+++ check_Valid_Email_Address.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class df56 {
+  public static void main(String[] args) {
+    String target = "neeraj@rediff.com";
+    Pattern p = Pattern.compile("[a-zA-Z0-9_.]+[@](gmail|yahoo|rediff)[.]com");
+    Matcher m = p.matcher(target);
+    int ctr = 0;
+    while(m.find())
+    {
+      System.out.println(m.start()+ "--"+m.end()+ "--"+m.group());
+      ctr++;
+    }
+    System.out.println(ctr);
+
+  }
+
+}
